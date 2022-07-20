6734d1dd3b1e4fdd83ff8d14eda128759deb5603
igor <varvarn1@gmail.com>
2022-07-20 10:13:55 +0300

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Igor Kononov on 20.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
